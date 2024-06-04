dcbcc5756171b6d752334606f4708bf7221d4c50
ankushnaik05 <ankushnaik765@gmail.com>
2024-06-04 16:51:43 +0530

    okay

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,42 +8,44 @@ export const About = () => {
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img
+        {/* <img
           src={getImageUrl("about/aboutImage.png")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
-        />
+        /> */}
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+              Passionate and skilled React Front End Developer with 2.7 years of experience in building and optimizing interactive, 
+              user-friendly web applications. Proficient in JavaScript, HTML5, CSS3, and modern libraries and frameworks such as 
+              React.js, Angular.js, and Material-UI. Seeking to leverage my expertise in responsive design, component-based architecture, and front-end development 
+              best practices to contribute to a dynamic development team and enhance user experiences at Fujitsu India
               </p>
             </div>
           </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-            <div className={styles.aboutItemText}>
+          {/* <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" /> 
+             <div className={styles.aboutItemText}>
               <h3>Backend Developer</h3>
               <p>
                 I have experience developing fast and optimised back-end systems
                 and APIs
               </p>
             </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+          </li> */}
+          {/* <li className={styles.aboutItem}>
+             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
               <h3>UI Designer</h3>
               <p>
                 I have designed multiple landing pages and have created design
                 systems as well
               </p>
-            </div>
-          </li>
+            </div> 
+          </li> */}
         </ul>
       </div>
     </section>
